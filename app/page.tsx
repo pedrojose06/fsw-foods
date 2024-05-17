7b5120d4d64401fe6c3052edf99f6004a95caf2f
Pedro <pmoraes@esapiens.com.br>
2024-05-17 20:12:23 -0300

    refactor: ensure product list receives products

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,25 @@ import Header from "./_components/header";
 import ProductList from "./_components/products-list";
 import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
+import { db } from "./_lib/prisma";
 
-const Home = () => {
+const Home = async () => {
+	const products = await db.product.findMany({
+		where: {
+			discountPercentage: {
+				gt: 0,
+			},
+		},
+		take: 10,
+		include: {
+			restaurant: {
+				select: {
+					name: true,
+				},
+			},
+		},
+	});
+
 	return (
 		<>
 			<Header />
@@ -39,7 +56,7 @@ const Home = () => {
 						<ChevronRightIcon size={16} />
 					</Button>
 				</div>
-				<ProductList />
+				<ProductList products={products} />
 			</div>
 		</>
 	);
